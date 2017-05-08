ae96441a1dadc5ce9602b44d1c0b99ec6c972192
xudafeng <xudafeng@126.com>
2017-05-08 14:27:53 +0800

    fixbug

--- XCTestWD/XCTestWDUITests/server/controllers/XCTestWDSourceController.swift
+++ XCTestWD/XCTestWDUITests/server/controllers/XCTestWDSourceController.swift
@@ -27,21 +27,21 @@ internal class XCTestWDSourceController: Controller {
     //MARK: Routing Logic Specification
     internal static func source(request: Swifter.HttpRequest) -> Swifter.HttpResponse {
         let temp = request.session?.application.tree()
-        return XCTestWDResponse.response(session: request.session, value: JSON(temp!))
+        return XCTestWDResponse.response(session: request.session, value: JSON(JSON(temp!).rawString() ?? ""))
     }
     
     internal static func sourceWithoutSession(request: Swifter.HttpRequest) -> Swifter.HttpResponse {
         let temp = XCTestWDSession.activeApplication()?.tree()
-        return XCTestWDResponse.response(session: request.session, value: JSON(JSON(temp!).rawString() ?? ""))
+        return XCTestWDResponse.response(session: request.session, value: JSON(temp!))
     }
     
     internal static func accessiblitySource(request: Swifter.HttpRequest) -> Swifter.HttpResponse {
         let temp = request.session?.application.accessibilityTree()
-        return XCTestWDResponse.response(session: request.session, value: JSON(temp!))
+        return XCTestWDResponse.response(session: request.session, value: JSON(JSON(temp!).rawString() ?? ""))
     }
     
     internal static func accessiblitySourceWithoutSession(request: Swifter.HttpRequest) -> Swifter.HttpResponse {
         let temp = XCTestWDSession.activeApplication()?.accessibilityTree()
-        return XCTestWDResponse.response(session: request.session, value: JSON(JSON(temp!).rawString() ?? ""))
+        return XCTestWDResponse.response(session: request.session, value: JSON(temp!))
     }
 }
