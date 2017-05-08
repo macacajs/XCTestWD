f7e56e27da46dcb84e3c16d8a314ed63fd51d4fc
zy.zhao <samuel.zhao.yue@live.com>
2017-05-08 13:49:08 +0800

    bug fix

--- XCTestWD/XCTestWDUITests/server/controllers/XCTestWDSourceController.swift
+++ XCTestWD/XCTestWDUITests/server/controllers/XCTestWDSourceController.swift
@@ -32,7 +32,7 @@ internal class XCTestWDSourceController: Controller {
     
     internal static func sourceWithoutSession(request: Swifter.HttpRequest) -> Swifter.HttpResponse {
         let temp = XCTestWDSession.activeApplication()?.tree()
-        return XCTestWDResponse.response(session: request.session, value: JSON(temp!))
+        return XCTestWDResponse.response(session: request.session, value: JSON(JSON(temp!).rawString() ?? ""))
     }
     
     internal static func accessiblitySource(request: Swifter.HttpRequest) -> Swifter.HttpResponse {
@@ -42,7 +42,6 @@ internal class XCTestWDSourceController: Controller {
     
     internal static func accessiblitySourceWithoutSession(request: Swifter.HttpRequest) -> Swifter.HttpResponse {
         let temp = XCTestWDSession.activeApplication()?.accessibilityTree()
-        XCTestWDXPath.findMatchesIn(XCTestWDSession.activeApplication()?.lastSnapshot() as! XCElementSnapshot, "//Other")
-        return XCTestWDResponse.response(session: request.session, value: JSON(temp!))
+        return XCTestWDResponse.response(session: request.session, value: JSON(JSON(temp!).rawString() ?? "")
     }
 }
