423c5aa5c7deb9cd69d2c19cbd66807fc5518861
SamuelZhaoY <samuel.zhao.yue@live.com>
2018-02-20 12:54:43 +0800

    modulization
    integration into existing module
    
    update aggregate script
    
    finish aggregation script, update ignore
    
    update ignore
    
    update projcect setting, dependent on fat framework
    
    update build settings
    
    revert shell
    
    update carthage
    
    rename project
    
    add Makefile
    
    update scheme
    
    make xcsheme shared
    
    update cartfile
    
    update carthafe
    
    update
    
    remove dependency setting in cartfile
    
    update dependency
    
    updare settings
    
    update

--- XCTestWD/XCTestWD/Server/Modules/Extensions/XCTestWDMathUtils.swift
+++ XCTestWD/XCTestWD/Server/Modules/Extensions/XCTestWDMathUtils.swift
@@ -24,17 +24,17 @@ class MathUtils {
         return actualSize;
     }
     
-    static func MD5(string: String) -> String {
-        let messageData = string.data(using:.utf8)!
-        var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
-        
-        _ = digestData.withUnsafeMutableBytes {digestBytes in
-            messageData.withUnsafeBytes {messageBytes in
-                CC_MD5(messageBytes, CC_LONG(messageData.count), digestBytes)
-            }
-        }
-        
-        return digestData.base64EncodedString()
-    }
+//    static func MD5(string: String) -> String {
+//        let messageData = string.data(using:.utf8)!
+//        var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
+//        
+//        _ = digestData.withUnsafeMutableBytes {digestBytes in
+//            messageData.withUnsafeBytes {messageBytes in
+//                CC_MD5(messageBytes, CC_LONG(messageData.count), digestBytes)
+//            }
+//        }
+//        
+//        return digestData.base64EncodedString()
+//    }
 
 }
