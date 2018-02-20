edd44dbb299f8e9c06d4a2b1cef4cf620f000420
SamuelZhaoY <samuel.zhao.yue@live.com>
2018-02-20 20:16:35 +0800

    finish aggregation script, update ignore

--- scripts/aggregate.sh
+++ scripts/aggregate.sh
@@ -3,35 +3,43 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Sets the target folders and the final framework product.
-FRAMEWORK_NAME=XCTestWDModule
+PROJECT_NAME=XCTestWD
 FRAMEWORK_CONFIG=Release
-
-# Get build number from plist
-echo "Get version..."
-mkdir -p "$SRCROOT/.build"
+BUILD_TARGET=XCTestWDModule
 
 # Install dir will be the final output to the framework.
 # The following line create it in the root folder of the current project.
-INSTALL_DIR="$SRCROOT/Frameworks/$FRAMEWORK_NAME.framework"
+INSTALL_DIR="$SRCROOT/Frameworks/$BUILD_TARGET.framework"
 
 # Working dir will be deleted after the framework creation.
 WORK_DIR="$SRCROOT/build"
-DEVICE_DIR="$WORK_DIR/${FRAMEWORK_CONFIG}-iphoneos/$FRAMEWORK_NAME.framework"
-SIMULATOR_DIR="$WORK_DIR/${FRAMEWORK_CONFIG}-iphonesimulator/$FRAMEWORK_NAME.framework"
+DEVICE_DIR="$WORK_DIR/${FRAMEWORK_CONFIG}-iphoneos/$BUILD_TARGET.framework"
+SIMULATOR_DIR="$WORK_DIR/${FRAMEWORK_CONFIG}-iphonesimulator/$BUILD_TARGET.framework"
 rm -rf "$WORK_DIR"
 
 echo "Building device..."
-xcodebuild -configuration "$FRAMEWORK_CONFIG" -target "$FRAMEWORK_NAME" -sdk iphoneos -project "$FRAMEWORK_NAME.xcodeproj" > /dev/null
-
-# backup file as Xcode 9 remove it in the next xcodebuild
-mv "$DEVICE_DIR" "$WORK_DIR/"
+xcodebuild -configuration "$FRAMEWORK_CONFIG" -target "$BUILD_TARGET" -sdk iphoneos -project "$PROJECT_NAME.xcodeproj" > /dev/null
 
 echo "Building simulator..."
-xcodebuild -configuration "$FRAMEWORK_CONFIG" -target "$FRAMEWORK_NAME" -sdk iphonesimulator -project "$FRAMEWORK_NAME.xcodeproj" > /dev/null
+xcodebuild -configuration "$FRAMEWORK_CONFIG" -target "$BUILD_TARGET" -sdk iphonesimulator -project "$PROJECT_NAME.xcodeproj" > /dev/null
 
 echo "Preparing directory..."
 rm -rf "$INSTALL_DIR"
 mkdir -p "$INSTALL_DIR/Headers"
+mkdir -p "$INSTALL_DIR/Modules/XCTestWDModule.swiftmodule"
 
-echo "Regulating Framework Deliverables Here:"
+# Regulating Framework Deliverables
+echo "Migrating System Headers:"
+cp "$SIMULATOR_DIR/Headers/"*.h "$INSTALL_DIR/Headers/"
 
+echo "Mixing Mutli-Architecture Swift Modules:"
+cp "$SIMULATOR_DIR/Modules/module.modulemap" "$INSTALL_DIR/Modules/"
+cp "$SIMULATOR_DIR/Modules/XCTestWDModule.swiftmodule/"* "$INSTALL_DIR/Modules/XCTestWDModule.swiftmodule/"
+cp "$DEVICE_DIR/Modules/XCTestWDModule.swiftmodule/"* "$INSTALL_DIR/Modules/XCTestWDModule.swiftmodule/"
+cp "$SIMULATOR_DIR/Info.plist" "$INSTALL_DIR/"
+
+echo "Combine Fat File"
+lipo -create "$DEVICE_DIR/$BUILD_TARGET" "$SIMULATOR_DIR/$BUILD_TARGET" -output "${INSTALL_DIR}/${BUILD_TARGET}"
+
+# Clean Up Intermediate File
+rm -rf "$WORK_DIR"
