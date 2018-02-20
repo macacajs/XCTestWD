15bc39b609a0e7055523dcfaac8b90da1a9b5b93
SamuelZhaoY <samuel.zhao.yue@live.com>
2018-02-20 12:54:43 +0800

    modulization

---
+++ XCTestWD/XCTestWDModule/XCTestWDModule.h
@@ -0,0 +1,30 @@
+//
+//  XCTestWDModule.h
+//  XCTestWDModule
+//
+//  Created by zhaoy on 20/02/2018.
+//  Copyright © 2018 XCTestWD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XCTestWDModule.
+FOUNDATION_EXPORT double XCTestWDModuleVersionNumber;
+
+//! Project version string for XCTestWDModule.
+FOUNDATION_EXPORT const unsigned char XCTestWDModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCTestWDModule/PublicHeader.h>
+
+#import <XCTestWDModule/CDStructures.h>
+#import <XCTestWDModule/XCUIElementQuery.h>
+#import <XCTestWDModule/XCUIElement.h>
+#import <XCTestWDModule/XCElementSnapshot.h>
+#import <XCTestWDModule/XCAXClient_iOS.h>
+#import <XCTestWDModule/XCUIApplication.h>
+#import <XCTestWDModule/XCTestWDApplication.h>
+#import <XCTestWDModule/XCTestPrivateSymbols.h>
+#import <XCTestWDModule/XCUICoordinate.h>
+#import <XCTestWDModule/XCTestDriver.h>
+#import <XCTestWDModule/XCTestDaemonsProxy.h>
+#import <XCTestWDModule/XCTRunnerDaemonSession.h>
