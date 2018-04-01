85dbc63b773ea403a60aeea1a2d3594cb713f599
xudafeng <xudafeng@126.com>
2018-04-01 13:03:01 +0800

    move to macaca-scripts

--- scripts/integration-testing.sh
+++ scripts/integration-testing.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-brew install ios-webkit-debug-proxy > /dev/null 2>&1
 brew install nvm > /dev/null 2>&1
 source $(brew --prefix nvm)/nvm.sh
 nvm install 8
 
-git clone https://github.com/macaca-sample/sample-nodejs.git --depth=1
-cd sample-nodejs
+npm i macaca-scripts -g
 
-npm i
-npm run test:ios
+macaca-scripts integration-test-ios
