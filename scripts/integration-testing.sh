13b2b20aba53559a5154460ccc181466b67b08d8
xudafeng <xudafeng@126.com>
2018-04-01 10:37:43 +0800

    update scripts

--- scripts/integration-testing.sh
+++ scripts/integration-testing.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-brew install node
+brew install ios-webkit-debug-proxy > /dev/null 2>&1
+brew install nvm > /dev/null 2>&1
+source $(brew --prefix nvm)/nvm.sh
+nvm install 8
 
 git clone https://github.com/macaca-sample/sample-nodejs.git --depth=1
-
 cd sample-nodejs
 
 npm i
-
 npm run test:ios
